21a3d5aba71dd2e176bb8cd85b0b4879b04a8186
arnu515 <5914253-arnu515@users.noreply.gitlab.com>
2022-09-27 03:35:52 +0530

    feat: add theme detection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,28 @@
 import "@fontsource/poppins/400.css";
 import type { AppProps } from "next/app";
-import { MantineProvider } from "@mantine/core";
+import { ColorSchemeProvider, MantineProvider } from "@mantine/core";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+  useEffect(() => {
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+    setTheme(prefersDark.matches ? "dark" : "light");
+  }, []);
+
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <ColorSchemeProvider
+      colorScheme={theme}
+      toggleColorScheme={(i) => i && setTheme(i)}
+    >
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ fontFamily: "Poppins, sans-serif", colorScheme: "dark" }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
 
