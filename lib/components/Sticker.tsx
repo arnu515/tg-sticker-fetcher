c4fe6c409b0fed4b9cd7adba9b8eb36be07adfb6
arnu515 <5914253-arnu515@users.noreply.gitlab.com>
2022-09-27 18:42:51 +0530

    feat: add

---
+++ lib/components/Sticker.tsx
@@ -0,0 +1,47 @@
+import type { StickerWithFile } from "$tg";
+import { Box, Text } from "@mantine/core";
+import { useEffect, useState } from "react";
+
+export default function Sticker({ sticker }: { sticker: StickerWithFile }) {
+  const [lottiePlayer, setLottiePlayer] = useState<any>(null);
+
+  useEffect(() => {
+    (async () => {
+      const LottiePlayer = (await import("./LottiePlayer")).default;
+      setLottiePlayer(
+        <LottiePlayer
+          url={`/api/tgfile?${new URLSearchParams({
+            file_path: sticker.file.file_path!,
+          }).toString()}`}
+        />
+      );
+    })();
+  }, [sticker]);
+
+  console.log(sticker.file);
+  if (!lottiePlayer) return <h1>loading</h1>;
+
+  return (
+    <Box
+      sx={(theme) => ({
+        position: "relative",
+        borderColor: theme.fn.primaryShade() as any,
+        borderRadius: "0.5rem",
+        borderWidth: "1px",
+        borderStyle: "solid",
+      })}
+    >
+      {lottiePlayer}{" "}
+      <Text
+        style={{
+          position: "absolute",
+          right: 10,
+          top: 10,
+          fontSize: "24px",
+        }}
+      >
+        {sticker.emoji}
+      </Text>
+    </Box>
+  );
+}
